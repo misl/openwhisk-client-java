34b91d1fe7c21f43f7631c1b895079339cfd477d
Minto van der Sluis <minto@xup.nl>
2017-09-03 20:59:47 +0200

    Initial commit Jave based Openwhisk API

---
+++ openwhisk-api-client-retrofit/src/test/java/SimpleTest.java
@@ -0,0 +1,14 @@
+import nl.xup.openwhisk.ApiClient;
+
+public class SimpleTest {
+
+  public static void main( String[] args ) {
+    // TODO Auto-generated method stub
+    final ApiClient client = new ApiClient( "35441018-3914-4861-a141-89a71ef4d3c2", "3rO4eV1ctVmWAiNyGlm4JEPbkKMd8mFNo3keLPu6CmfL4xU73TY8vWtsxlZgpFP3" );
+    
+//    client.setBasePath( "https://openwhisk.ng.bluemix.net:443/api/v1/" );
+//    client.buildClient( ActionsApi.class );
+
+  }
+
+}
